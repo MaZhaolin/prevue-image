6a0f4362671e37260108eda1b0303fc70aa587c3
insertSweat <769356353@qq.com>
2019-02-19 17:49:23 +0800

    update: use cached function

--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,13 @@ export const extendStyle = (to, _from) => {
   return to
 }
 
+const cached = fn => {
+  let cache = undefined
+  return () => {
+    return typeof cache === 'undefined' ? (cache = fn()) : cache
+  }
+}
+
 export const hasScrollbar = () => {
   return (
     document.body.scrollHeight >
@@ -12,13 +19,12 @@ export const hasScrollbar = () => {
   )
 }
 
-export const getScrollbarWidth = () => {
+export const getScrollbarWidth = cached(() => {
   const scrollDiv = document.createElement('div')
   scrollDiv.style.cssText =
     'width: 99px; height: 99px; overflow: scroll; position: absolute; top: -9999px;'
   document.body.appendChild(scrollDiv)
   const scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth
   document.body.removeChild(scrollDiv)
-
   return scrollbarWidth
-}
+})
